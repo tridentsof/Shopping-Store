c4328b7ca9038385ad8b0c68a5c82139230d34df
tridentsof <gahanhthao@gmail.com>
2021-10-01 10:35:26 +0700

    Tri chinh sua dataAccess

--- shopMobileOnline/DataAccess.cs
+++ shopMobileOnline/DataAccess.cs
@@ -9,7 +9,7 @@ namespace shopMobileOnline
 {
     public class DataAccess
     {
-        private SqlConnection connection;
+        public SqlConnection connection;
         public void MoKetNoiCSDL()
         {
             connection = new SqlConnection();
@@ -34,6 +34,103 @@ namespace shopMobileOnline
         {
             if (connection.State == ConnectionState.Open)
                 connection.Close();
+        }
+
+        public SqlCommand BuildCommand(string procedureName, IDataParameter[] parameters)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = procedureName;
+            cmd.Connection = connection;
+            cmd.CommandType = CommandType.StoredProcedure;
+            foreach (SqlParameter parameter in parameters)
+            {
+                cmd.Parameters.Add(parameter);
+            }
+            return cmd;
+        }
+
+        public SqlCommand BuildIntCommand(string procedureName, IDataParameter[] parameters)
+        {
+            SqlCommand cmd = BuildCommand(procedureName, parameters);
+            cmd.Parameters.Add(new SqlParameter("ReturnValue",
+                SqlDbType.Int,
+                4,
+                ParameterDirection.ReturnValue,
+                false,
+                0, 0,
+                string.Empty,
+                DataRowVersion.Default,
+                null));
+            return cmd;
+        }
+
+        public int ExecuteIntCommand(string procedureName, IDataParameter[] parameters)
+        {
+            try
+            {
+                int returnValue;
+                MoKetNoiCSDL();
+                SqlCommand cmd = BuildIntCommand(procedureName, parameters);
+                cmd.ExecuteNonQuery();
+                returnValue = (int)cmd.Parameters["ReturnValue"].Value;
+                DongKetNoiCSDL();
+                return returnValue;
+            }
+            catch
+            {
+                throw new Exception("ExecuteIntCommand Error");
+            }
+        }
+
+        public int ExecuteNonQuery(string procedureName, IDataParameter[] parameters)
+        {
+            try
+            {
+                MoKetNoiCSDL();
+                SqlCommand cmd = BuildCommand(procedureName, parameters);
+                cmd.CommandType = CommandType.StoredProcedure;
+                int rowsAffected = cmd.ExecuteNonQuery();
+                DongKetNoiCSDL();
+                return rowsAffected;
+            }
+            catch (Exception)
+            {
+                throw new Exception("ExecuteNonQuery Error!");
+            }
+        }
+
+        public  DataTable ExecuteQuery(string procedureName, IDataParameter[] parameters)
+        {
+            try
+            {
+                DataSet dataSet = new DataSet();
+                MoKetNoiCSDL();
+                SqlDataAdapter sqlDA = new SqlDataAdapter();
+                sqlDA.SelectCommand = BuildCommand(procedureName, parameters);
+                sqlDA.Fill(dataSet);
+                DongKetNoiCSDL();
+                return dataSet.Tables[0];
+            }
+            catch (Exception exp)
+            {
+                throw exp;
+            }
+        }
+
+        public object ExecuteScalar(string procedureName, IDataParameter[] parameters)
+        {
+            try
+            {
+                SqlCommand cmd = BuildCommand(procedureName, parameters);
+                MoKetNoiCSDL();
+                object retValue = cmd.ExecuteScalar();
+                DongKetNoiCSDL();
+                return retValue;
+            }
+            catch (Exception exp)
+            {
+                throw new Exception("Execute Scalar Error!", exp);
+            }
         }
     }
 }
