7e694454a6d517cae12d12971485c758b5600710
NGUYEN DO <vandohtk@gmail.com>
2021-10-04 20:35:15 +0700

    Do tao filter va phan trang

--- shopMobileOnline/Admin/AD_Loc_SP.aspx.cs
+++ shopMobileOnline/Admin/AD_Loc_SP.aspx.cs
@@ -1,51 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Web;
+using System.Data;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Globalization;
-using System.Linq;
-using System.Text.RegularExpressions;
 
 
 namespace shopMobileOnline.Admin
 {
-    public partial class QLSanPham : System.Web.UI.Page
+    public partial class AD_Loc_SP : System.Web.UI.Page
     {
-        int pagesize = 7;
-        int page = 0;
-        int totalpage = 0;
         protected void Page_Load(object sender, EventArgs e)
         {
            
+            string strSearch = Request.QueryString.Get("search2");
+            
+            if (Session["id"] == null)
+            {
+                Response.Redirect("ADLogin.aspx");
+            }
+            //hien thi ten nguoi dung o goc tren phai
+            else
+            {
+
+                
+
+            }
+
             if (!IsPostBack)
             {
-                if (Session["id"] == null)
-                {
-                    Response.Redirect("ADLogin.aspx");
-                }
-                //hien thi ten nguoi dung o goc tren phai
-                else
-                {
-                    this.Master.ADName = Session["id"].ToString();
-                    this.Master.ADName1 = Session["id"].ToString();
-
-                }
                 DataAccess dataAccess = new DataAccess();
 
                 dataAccess.MoKetNoiCSDL();
 
+                String sql = "";
+                if (strSearch != null)
+                {
+                    sql = "SELECT ID_SP, TENSP, TENNSX, TENLOAI, DONGIA, SOLUONG_DABAN, CASE TINHTRANG WHEN 1 THEN N'Mở' ELSE N'Đóng' END AS TINHTRANG, 'TrangCapNhatSP.aspx?idSP=' + CAST(ID_SP AS NVARCHAR) AS CAPNHAT, ID_SP AS XOA FROM [SANPHAM], [NHASANXUAT], [LOAI] WHERE ( [SANPHAM].ID_NSX = [NHASANXUAT].ID_NSX AND [SANPHAM].ID_LOAI = [LOAI].ID_LOAI AND [NHASANXUAT].TENNSX like '%" + @strSearch + "%') OR ( [SANPHAM].ID_NSX = [NHASANXUAT].ID_NSX AND [SANPHAM].ID_LOAI = [LOAI].ID_LOAI AND [SANPHAM].TENSP like '%" + @strSearch + "%') OR ( [SANPHAM].ID_NSX = [NHASANXUAT].ID_NSX AND [SANPHAM].ID_LOAI = [LOAI].ID_LOAI AND [LOAI].TENLOAI like '%" + @strSearch + "%') OR ( [SANPHAM].ID_NSX = [NHASANXUAT].ID_NSX AND [SANPHAM].ID_LOAI = [LOAI].ID_LOAI AND TINHTRANG like '%" + @strSearch + "%')";
+                }
+                else
+                {
+                    sql = "SELECT ID_SP, TENSP, TENNSX, TENLOAI, DONGIA, SOLUONG_DABAN, CASE TINHTRANG WHEN 1 THEN N'Mở' ELSE N'Đóng' END AS TINHTRANG, 'TrangCapNhatSP.aspx?idSP=' + CAST(ID_SP AS NVARCHAR) AS CAPNHAT, ID_SP AS XOA FROM [SANPHAM], [NHASANXUAT], [LOAI] WHERE [SANPHAM].ID_NSX = [NHASANXUAT].ID_NSX AND [SANPHAM].ID_LOAI = [LOAI].ID_LOAI";
+                }
                 //Lay du lieu tu dtb luu vao dataTable
 
-                string sql = "SELECT TOP(7) ID_SP, TENSP, TENNSX, TENLOAI, DONGIA, SOLUONG_DABAN, CASE TINHTRANG WHEN 1 THEN N'Mở' ELSE N'Đóng' END AS TINHTRANG, 'TrangCapNhatSP.aspx?idSP=' + CAST(ID_SP AS NVARCHAR) AS CAPNHAT, ID_SP AS XOA FROM [SANPHAM], [NHASANXUAT], [LOAI] WHERE [SANPHAM].ID_NSX = [NHASANXUAT].ID_NSX AND [SANPHAM].ID_LOAI = [LOAI].ID_LOAI ORDER BY SANPHAM.ID_SP  ";
+
                 DataTable dtSP = dataAccess.LayBangDuLieu(sql);
 
-                //Tao html dong duoi dang string de tao bang show du lieu 
-
+                //Tao html dong duoi dang string de tao bang show du lieu
                 if (dtSP != null && dtSP.Rows.Count > 0)
                 {
 
@@ -103,32 +107,22 @@ namespace shopMobileOnline.Admin
                     //    connection.Close();
                     dataAccess.DongKetNoiCSDL();
                 }
-
             }
-            //check xem nguoi dung co dang trong phien dang nhap
-           
-        }   
-        protected void btn_Loc_Click(object sender, EventArgs e)
-        {
-            Response.Redirect("AD_Loc_SP.aspx?search2="+ txt_Loc.Text.Trim());
         }
 
-      
-
-        protected void Button1_Click(object sender, EventArgs e)
+        protected void LinkButton1_Click(object sender, EventArgs e)
         {
-            Label1.Text = "1";
             DataAccess dataAccess = new DataAccess();
 
             dataAccess.MoKetNoiCSDL();
 
+
             //Lay du lieu tu dtb luu vao dataTable
 
-            string sql = "SELECT ID_SP, TENSP, TENNSX, TENLOAI, DONGIA, SOLUONG_DABAN, CASE TINHTRANG WHEN 1 THEN N'Mở' ELSE N'Đóng' END AS TINHTRANG, 'TrangCapNhatSP.aspx?idSP=' + CAST(ID_SP AS NVARCHAR) AS CAPNHAT, ID_SP AS XOA FROM [SANPHAM], [NHASANXUAT], [LOAI] WHERE [SANPHAM].ID_NSX = [NHASANXUAT].ID_NSX AND [SANPHAM].ID_LOAI = [LOAI].ID_LOAI ORDER BY SANPHAM.ID_SP offset 0 rows fetch next 7 rows only ";
+            string sql = "SELECT ID_SP, TENSP, TENNSX, TENLOAI, DONGIA, SOLUONG_DABAN, CASE TINHTRANG WHEN 1 THEN N'Mở' ELSE N'Đóng' END AS TINHTRANG, 'TrangCapNhatSP.aspx?idSP=' + CAST(ID_SP AS NVARCHAR) AS CAPNHAT, ID_SP AS XOA FROM [SANPHAM], [NHASANXUAT], [LOAI] WHERE[SANPHAM].ID_NSX = [NHASANXUAT].ID_NSX AND [SANPHAM].ID_LOAI = [LOAI].ID_LOAI ORDER BY LOAI.TENLOAI  ";
             DataTable dtSP = dataAccess.LayBangDuLieu(sql);
 
-            //Tao html dong duoi dang string de tao bang show du lieu 
-
+            //Tao html dong duoi dang string de tao bang show du lieu
             if (dtSP != null && dtSP.Rows.Count > 0)
             {
 
@@ -188,20 +182,19 @@ namespace shopMobileOnline.Admin
             }
         }
 
-        protected void Button2_Click(object sender, EventArgs e)
+        protected void LinkButton2_Click(object sender, EventArgs e)
         {
-            Label1.Text = "2";
             DataAccess dataAccess = new DataAccess();
 
             dataAccess.MoKetNoiCSDL();
 
+
             //Lay du lieu tu dtb luu vao dataTable
 
-            string sql = "SELECT ID_SP, TENSP, TENNSX, TENLOAI, DONGIA, SOLUONG_DABAN, CASE TINHTRANG WHEN 1 THEN N'Mở' ELSE N'Đóng' END AS TINHTRANG, 'TrangCapNhatSP.aspx?idSP=' + CAST(ID_SP AS NVARCHAR) AS CAPNHAT, ID_SP AS XOA FROM [SANPHAM], [NHASANXUAT], [LOAI] WHERE [SANPHAM].ID_NSX = [NHASANXUAT].ID_NSX AND [SANPHAM].ID_LOAI = [LOAI].ID_LOAI ORDER BY SANPHAM.ID_SP offset 7 rows fetch next 7 rows only ";
+            string sql = "SELECT ID_SP, TENSP, TENNSX, TENLOAI, DONGIA, SOLUONG_DABAN, CASE TINHTRANG WHEN 1 THEN N'Mở' ELSE N'Đóng' END AS TINHTRANG, 'TrangCapNhatSP.aspx?idSP=' + CAST(ID_SP AS NVARCHAR) AS CAPNHAT, ID_SP AS XOA FROM [SANPHAM], [NHASANXUAT], [LOAI] WHERE[SANPHAM].ID_NSX = [NHASANXUAT].ID_NSX AND [SANPHAM].ID_LOAI = [LOAI].ID_LOAI ORDER BY SANPHAM.DONGIA   ";
             DataTable dtSP = dataAccess.LayBangDuLieu(sql);
 
-            //Tao html dong duoi dang string de tao bang show du lieu 
-
+            //Tao html dong duoi dang string de tao bang show du lieu
             if (dtSP != null && dtSP.Rows.Count > 0)
             {
 
@@ -261,20 +254,19 @@ namespace shopMobileOnline.Admin
             }
         }
 
-        protected void Button3_Click(object sender, EventArgs e)
+        protected void LinkButton3_Click(object sender, EventArgs e)
         {
-            Label1.Text = "3";
             DataAccess dataAccess = new DataAccess();
 
             dataAccess.MoKetNoiCSDL();
 
+
             //Lay du lieu tu dtb luu vao dataTable
 
-            string sql = "SELECT ID_SP, TENSP, TENNSX, TENLOAI, DONGIA, SOLUONG_DABAN, CASE TINHTRANG WHEN 1 THEN N'Mở' ELSE N'Đóng' END AS TINHTRANG, 'TrangCapNhatSP.aspx?idSP=' + CAST(ID_SP AS NVARCHAR) AS CAPNHAT, ID_SP AS XOA FROM [SANPHAM], [NHASANXUAT], [LOAI] WHERE [SANPHAM].ID_NSX = [NHASANXUAT].ID_NSX AND [SANPHAM].ID_LOAI = [LOAI].ID_LOAI ORDER BY SANPHAM.ID_SP offset 14 rows fetch next 7 rows only ";
+            string sql = "SELECT ID_SP, TENSP, TENNSX, TENLOAI, DONGIA, SOLUONG_DABAN, CASE TINHTRANG WHEN 1 THEN N'Mở' ELSE N'Đóng' END AS TINHTRANG, 'TrangCapNhatSP.aspx?idSP=' + CAST(ID_SP AS NVARCHAR) AS CAPNHAT, ID_SP AS XOA FROM [SANPHAM], [NHASANXUAT], [LOAI] WHERE[SANPHAM].ID_NSX = [NHASANXUAT].ID_NSX AND [SANPHAM].ID_LOAI = [LOAI].ID_LOAI ORDER BY SANPHAM.TINHTRANG";
             DataTable dtSP = dataAccess.LayBangDuLieu(sql);
 
-            //Tao html dong duoi dang string de tao bang show du lieu 
-
+            //Tao html dong duoi dang string de tao bang show du lieu
             if (dtSP != null && dtSP.Rows.Count > 0)
             {
 
@@ -334,5 +326,4 @@ namespace shopMobileOnline.Admin
             }
         }
     }
-   
 }
