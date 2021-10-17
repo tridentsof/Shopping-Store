d9029a2a0cf33b4d01d5930492081ff3fb5e6ba1
TienDat174 <nguyentiendat.tk12@gmail.com>
2021-10-17 09:03:43 +0700

    Sua Loi Nho

--- shopMobileOnline/Admin/DoanhThuDenNgay.aspx.designer.cs
+++ shopMobileOnline/Admin/DoanhThuDenNgay.aspx.designer.cs
@@ -42,15 +42,6 @@ namespace shopMobileOnline.Admin
         protected global::System.Web.UI.WebControls.Button Button1;
 
         /// <summary>
-        /// table1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlTable table1;
-
-        /// <summary>
         /// Panel1 control.
         /// </summary>
         /// <remarks>
